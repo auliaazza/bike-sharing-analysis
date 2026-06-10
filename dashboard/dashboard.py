8834edec72fb49ea5701c2bd1588a9f6ba9cd3ea
Aulia Azzahra <auliaazzahra422@gmail.com>
2026-06-10 18:29:37 +0700

    Fix variable name for selected season in dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -37,7 +37,7 @@ start_date, end_date = st.sidebar.date_input(
 
 # FILTER BY SEASON
 list_season = df['season_day'].unique()
-selected_season = st.sidebar.multiselect(
+selected_season_day = st.sidebar.multiselect(
     "Select Season",
     opttions=list_season,
     default=list_season
