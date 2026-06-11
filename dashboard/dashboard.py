0043f422dabd1eee2e005d8a3efbc82539ec4263
Aulia Azzahra <auliaazzahra422@gmail.com>
2026-06-11 15:20:28 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -159,7 +159,7 @@ with tab1:
         st.subheader("Hourly Usage Trends by Day Type")
         st.bar_chart(
                 data=hourly_pivot,
-                color=color=["#b0bec5", "#1d2a62"],
+                color=["#b0bec5", "#1d2a62"],
                 x_label="Hour of the Day",
                 y_label="Average Rentals"
             )
