3100baebde6b883430f6b76bb841c9d9cd1d47a6
Aulia Azzahra <auliaazzahra422@gmail.com>
2026-06-11 14:42:20 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -166,14 +166,18 @@ with tab1:
     
     col1, col2, col3 = st.columns(3)
     #TEMPERATURE IMPACT
-    with col1:
-        st.subheader("Impact of Temperature on Daily Rentals")
-        st.scatter_chart(
-                data=df,
-                x='temp_norm_day',
-                y='cnt_day',
-                color='demand_level',
-                color_discrete_sequence=['#1d2a62', '#2496B4', '#b0bec5'], # Urutan warna dari biru tua ke abu-abu
-                x_label='Normalized Temperature',
-                y_label='Total Daily Rentals'
-            )
+    st.subheader("Impact of Temperature on Daily Rentals")
+    fig = px.scatter(
+                df,
+                x="temp_norm_day",    # Sumbu X: Suhu yang sudah dinormalisasi
+                y="cnt_day",         # Sumbu Y: Total rental harian
+                color="demand_level", # Titik-titik akan berwarna berdasarkan tingkat permintaan
+                color_discrete_sequence=['#1d2a62', '#2496B4', '#b0bec5'], # Skema warna biru tua kamu
+                labels={
+                    "temp_norm_day": "Normalized Temperature",
+                    "cnt_day": "Total Daily Rentals",
+                    "demand_level": "Demand Level"
+                },
+                opacity=0.7          # Membuat titik agak transparan agar penumpukan data terlihat
+                )
+        st.plotly_chart(fig, use_container_width=True)
