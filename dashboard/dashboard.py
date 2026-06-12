5c3005a7e9e823aade48ab63d72f04b8df6ac8a9
Aulia Azzahra <auliaazzahra422@gmail.com>
2026-06-12 19:06:27 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -213,7 +213,7 @@ with tab1:
     with col1:
         st.subheader("Impact of Temperature on Daily Rentals")
         fig = px.scatter(
-            data=filtered_df,
+            data_frame=filtered_df,
             x='temp_norm_day',
             y='cnt_day',
             color='season',
