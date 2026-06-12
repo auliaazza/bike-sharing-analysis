873bd1e451074c2c8b3bbe4b39c4e6e55fb92cf7
Aulia Azzahra <auliaazzahra422@gmail.com>
2026-06-12 16:27:41 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -185,7 +185,7 @@ with tab1:
             sizes = [total_casual, total_registered]
             colors = ['#b0bec5', '#1d2a62']
 
-        fig, ax = plt.subplots(figsize=(5, 4))
+        fig, ax = plt.subplots(figsize=(7, 5))
         
         wedges, texts, autotexts = ax.pie(
             sizes, 
