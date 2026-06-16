06c0e4d7e08303fdab1a265d063273be1114275b
Aulia Azzahra <auliaazzahra422@gmail.com>
2026-06-16 13:31:43 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -73,6 +73,13 @@ with st.sidebar:
         label_visibility="collapsed"
     )
 
+    st.markdown(" ### Connect with Me")
+    col1, col2 = st.columns (2)
+    with col1:
+        st.page_link("www.linkedin.com/in/azzahraa248", label="LinkedIn", icon="linkedin")
+    with col2:
+        st.page_link("https://github.com/auliaazza/bike-sharing-analysis", label="Github", icon="github")
+
 # Filtered Data
 filtered_df = df[(df['dteday'].dt.date >= start_date) & (df['dteday'].dt.date <= end_date)]
 
